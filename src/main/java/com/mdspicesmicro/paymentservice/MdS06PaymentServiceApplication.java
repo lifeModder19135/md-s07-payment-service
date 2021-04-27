839cde51239fbc991c5235cef682e94ebdc234c0
Nate Tolbert <ntolbertu85@gmail.com>
2021-04-27 03:32:41 -0400

    04272021 -- Project/Global -- INITIAL COMMIT - Project freshly extracted from Initializr;

---
+++ src/main/java/com/mdspicesmicro/paymentservice/MdS06PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdS06PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdS06PaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdspicesmicro/paymentservice/MdS06PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdS06PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
